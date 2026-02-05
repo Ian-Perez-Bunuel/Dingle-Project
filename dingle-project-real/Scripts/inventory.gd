9859aafa49b3f2fadc26d6e2ca2f88e0419c974e
Ian Perez Bunuel <148461774+Ian-PB@users.noreply.github.com>
2026-02-05 20:37:13 +0000

    Ui stuff

---
+++ dingle-project-real/Scripts/inventory.gd
@@ -0,0 +1,12 @@
+extends Control
+@onready var inventory_panel := $"."
+
+func _ready() -> void:
+	inventory_panel.visible = false
+	
+func toggle_show():
+	inventory_panel.visible = !inventory_panel.visible
+	
+func _process(delta: float) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		toggle_show()
